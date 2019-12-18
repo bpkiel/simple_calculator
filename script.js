3d3e4f1882daf560d1b19786a8035a44ca8bccda
Ben Kiel <bpkiel@yahoo.com>
2019-12-17 23:35:18 -0800

    add requirements notes to js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //Requirements
-// 1.  Display value of buttons clicked in the screen
-// 2.  If "C" is clicked, reset the screen to 0
+// 1.  Display value of buttons clicked in the screen:
+//          a. display in screen each number clicked before operator is clicked
+//          b. upon clicking operator:
+//              store first number
+//          c. upon clicking another number
+//              store operator
+//              display second number(s) on screen
+//          d. upon clicking equals
+//              return result
+// 2.  If "C" is clicked:
+//         reset the screen to 0
+//         remove previously set number 1 and operator, if applicable
 // 3.  If backspace is clicked, remove the last button value from the screen
 // 4.  If equal is clicked, run a function that performs the operation on the two numbers
 
