c40b7c879b3bee3586368434623759b7e7f60305
Ben Kiel <bpkiel@yahoo.com>
2019-12-29 16:24:37 -0800

    Finalized first draft

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 
 
+// Let's go get access to the buttons (and the screen) we need:
+
 let screen = document.getElementById('screen');
-
-//Section to handle number buttons
-
 let numBtn = document.getElementsByClassName('num-button');
+let operatorBtn = document.getElementsByClassName('operator-btn');
+let backSpace = document.getElementById('backspace-btn');
+let clear = document.getElementById('clear-btn');
+let equalsButton = document.getElementById('equals-btn');
+
+// Set up initial event listeners:
 
 for (let btns of numBtn) {
     btns.addEventListener('click', firstNum);
 }
-
-//section to handle operator buttons and save first number
-
-let operatorBtn = document.getElementsByClassName('opBtn');
-
 for (let btns of operatorBtn) {
     btns.addEventListener('click', saveOperator)
 }
+backSpace.addEventListener('click', backspace);
+clear.addEventListener('click', clearAll);
+equalsButton.addEventListener('click', runCalc);
+
+// Functions to save first and second numbers to arrays:
+
+let firstNumberArray = [];
+
+let secondNumberArray = [];
+
+function firstNum(e) {
+    if (firstNumberArray.length < 8) {
+        firstNumberArray.push(e.target.innerHTML);
+        screen.innerHTML = firstNumberArray.join('');
+        console.log('FirstArray:',firstNumberArray);
+    }
+}
+
+function secondNum(e) {
+    screen.innerHTML = 0;
+    if (secondNumberArray.length < 8) {
+        secondNumberArray.push(e.target.innerHTML);
+        screen.innerHTML = secondNumberArray.join('');
+        console.log('SecondArray:',secondNumberArray);
+    }
+}
+
+//  Function to save operator, turn first array into number, and set functions to receive second number:
 
 function saveOperator(e) {
     operatorValue = e.target.innerHTML;
+
     if (firstNumberArray.length > 0) {
-        firstNumFinal = firstNumberArray.join('');
+        firstNumFinal = parseInt(firstNumberArray.join(''));
     } else {
         firstNumFinal = 0;
     }
-    operatorColor = e.target.style.backgroundColor = 'black'
+
+    operatorColor = e.target.style.backgroundColor = 'darkOrange';
 
      for (let btns of numBtn) {
         btns.removeEventListener('click', firstNum);
@@ -38,37 +68,9 @@ function saveOperator(e) {
     }
 }
 
-//Section to handle numbers
+//  Function to run backspace button:
 
-let firstNumberArray = [];
-
-let secondNumberArray = [];
-
-function firstNum(e) {
-    if (firstNumberArray.length < 8) {
-        firstNumberArray.push(e.target.innerHTML);
-        screen.innerHTML = firstNumberArray.join('');
-        console.log('first function');
-        console.log('FirstArray:',firstNumberArray);
-    }
-}
-function secondNum(e) {
-    screen.innerHTML = 0;
-    if (secondNumberArray.length < 8) {
-        secondNumberArray.push(e.target.innerHTML);
-        screen.innerHTML = secondNumberArray.join('');
-        console.log('second function');
-        console.log('SecondArray:',secondNumberArray);
-    }
-}
-
-//  Section to handle backspace button
-
-let backSpace = document.getElementById('backspace');
-
-backSpace.addEventListener('click', removeOne)
-
-function removeOne() {
+function backspace() {
     firstNumberArray.pop();
     if (firstNumberArray.length > 0) {
         screen.innerHTML = firstNumberArray.join('');
@@ -77,11 +79,7 @@ function removeOne() {
     }
 }
 
-// Section to handle clear button
-
-let clear = document.getElementById('clear');
-
-clear.addEventListener('click', clearAll);
+// Function to run clear button:
 
 function clearAll() {
     screen.innerHTML = 0;
@@ -102,18 +100,15 @@ function clearAll() {
     }
 }
 
-//section to handle equals button
-
-let equalsButton = document.getElementById('equals');
-
-equalsButton.addEventListener('click', runCalc);
+// Function for equals button to run equation on both numbers, display result, and set result as new first number:
 
 let finalValue;
+let secondNumFinal;
 
 function runCalc() {
 
     if (secondNumberArray.length > 0) {
-        secondNumFinal = secondNumberArray.join('');
+        secondNumFinal = parseInt(secondNumberArray.join(''));
     } else {
         secondNumFinal = 0;
     }
@@ -122,29 +117,34 @@ function runCalc() {
         btns.removeEventListener('click', saveOperator);
     }
 
-    if (operatorValue === 'x') {
-       finalValue = firstNumFinal * secondNumFinal;
-       screen.innerHTML = finalValue;
-    } else if (operatorValue === '/') {
-           let finalValue = firstNumFinal / secondNumFinal;
-           screen.innerHTML = finalValue;
-        } else if (operatorValue === '-') {
-               let finalValue = screen.innerHTML = firstNumFinal - secondNumFinal;
-            } else if (operatorValue === '+') {
-                   let finalValue = screen.innerHTML = parseInt(firstNumFinal) + parseInt(secondNumFinal);
-                }
-    // firstNumberArray = finalValue.split('');
-    firstNumberArray = [];
+    switch(operatorValue) {
+        case 'x':
+            finalValue = firstNumFinal * secondNumFinal;
+            screen.innerHTML = finalValue;
+            break;
+        case '/':
+            finalValue = firstNumFinal / secondNumFinal;
+            screen.innerHTML = finalValue;
+            break;
+        case '-':
+            finalValue = firstNumFinal - secondNumFinal;
+            screen.innerHTML = finalValue;
+            break;
+        case '+':
+            finalValue = parseInt(firstNumFinal) + parseInt(secondNumFinal);
+            screen.innerHTML = finalValue;
+            break;
+    }
+
+    firstNumberArray = finalValue.toString().split('');
     secondNumberArray = [];
     secondNumFinal = '';
-    operatorValue = '';
-    firstNumFinal = '';
 
     for (let btns of numBtn) {
-        btns.addEventListener('click', firstNum);
+        btns.removeEventListener('click', firstNum);
     }
     for (let btns of numBtn) {
-        btns.removeEventListener('click', secondNum);
+        btns.addEventListener('click', secondNum);
     }
     for (let btns of operatorBtn) {
         btns.addEventListener('click', saveOperator);
