b95d74b1190148303a981abfc6d6b8dcc0d8d052
Ben Kiel <bpkiel@yahoo.com>
2020-01-09 19:55:33 -0800

    create resets file for review

---
+++ resets.js
@@ -0,0 +1,38 @@
+
+
+function resetButtonsAfterClear() {
+    for (let btns of numBtn) {
+        btns.addEventListener('click', firstNum);
+    }
+    for (let btns of numBtn) {
+        btns.removeEventListener('click', secondNum);
+    }
+    for (let btns of operatorBtn) {
+        btns.addEventListener('click', saveOperator);
+        btns.style.backgroundColor = 'orange';
+    }
+}
+
+function resetButtonsAfterOperator() {
+     for (let btns of numBtn) {
+        btns.removeEventListener('click', firstNum);
+     }
+     for (let btns of numBtn) {
+         btns.addEventListener('click', secondNum);
+     }
+}
+
+function resetButtonsAfterCalc() {
+    for (let btns of numBtn) {
+        btns.removeEventListener('click', firstNum);
+    }
+    for (let btns of numBtn) {
+        btns.addEventListener('click', secondNum);
+    }
+    for (let btns of operatorBtn) {
+        btns.addEventListener('click', saveOperator);
+        btns.style.backgroundColor = 'orange';
+    }
+}
+
+export { resetButtonsAfterOperator, resetButtonsAfterClear, resetButtonsAfterCalc };
